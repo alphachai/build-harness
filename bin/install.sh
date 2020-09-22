eb2435023cace5898f29b1604f09a858690d5ded
Charlie Mathews <charlie@charliemathews.com>
2020-09-22 14:01:25 -0500

    Add docker network targets and set org to alphachai

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-export BUILD_HARNESS_ORG=${1:-mintel}
+export BUILD_HARNESS_ORG=${1:-alphachai}
 export BUILD_HARNESS_PROJECT=${2:-build-harness}
 export BUILD_HARNESS_BRANCH=${3:-master}
 export GITHUB_REPO="https://github.com/${BUILD_HARNESS_ORG}/${BUILD_HARNESS_PROJECT}.git"
